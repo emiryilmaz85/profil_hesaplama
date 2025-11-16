289c0b6561de97d2273fe818fa9b988b1492a62e
emiryilmaz85 <emir947947@gmail.com>
2025-11-16 23:14:25 +0300

    Add files via upload

---
+++ tables/bulbflat.py
@@ -0,0 +1,7 @@
+
+# -*- coding: utf-8 -*-
+BULBFLAT_TABLO = [
+    {"profil": "BF 80x7",   "B": 80.0,  "t": 7.0},
+    {"profil": "BF 100x7",  "B": 100.0, "t": 7.0},
+    {"profil": "BF 120x8",  "B": 120.0, "t": 8.0},
+]
---
+++ tables/square.py
@@ -0,0 +1,8 @@
+
+# -*- coding: utf-8 -*-
+SQUARE_TABLO = [
+    {"profil": "20x20", "a": 20.0},
+    {"profil": "30x30", "a": 30.0},
+    {"profil": "40x40", "a": 40.0},
+    {"profil": "50x50", "a": 50.0},
+]
